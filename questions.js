43338bd46dab5c55b26d31406dcc6138725865a7
Artsiom Mitskevitsh <87974923+flexinnext@users.noreply.github.com>
2023-05-21 08:39:17 +0200

    Quiz

---
+++ questions.js
@@ -0,0 +1,58 @@
+let jsq_questions = [
+    {
+        "id": 1,
+        "question": "Which of the following is the correct syntax to redirect a URL using JavaScript?",
+        "options": {
+            "a": "window.location='http://www.google.com';",
+            "b": "document.location='http://www.google.com';",
+            "c": "browser.location='http://www.google.com';",
+            "d": "navigator.location='http://www.google.com';"
+        },
+        "answer": "a"
+    },
+    {
+        "id": 2,
+        "question": "Inside which HTML element do we put the JavaScript?",
+        "options": {
+            "a": "<javascript>",
+            "b": "\x3C/script>",
+            "c": "<java>",
+            "d": "<js>"
+        },
+        "answer": "b"
+    },
+    {
+        "id": 3,
+        "question": "Which of the following is a valid JavaScript file extension?",
+        "options": {
+            "a": ".java",
+            "b": ".script",
+            "c": ".js",
+            "d": ".javascript"
+        },
+        "answer": "c"
+    },
+    {
+        "id": 4,
+        "question": " What does CSS stand for?",
+        "options": {
+            "a": "Computer Style Sheet",
+            "b": "Common Style Sheet",
+            "c": "Colorful Style Sheet",
+            "d": "Cascading Style Sheet"
+        },
+        "answer": "d"
+    },
+    {
+        "id": 5,
+        "question": "what will be the output of this code?",
+        "code": "console.log(typeof typeof 1);",
+        "options": {
+            "a": "number",
+            "b": "1",
+            "c": "string",
+            "d": "true"
+        },
+        "answer": "c"
+    }
+];
